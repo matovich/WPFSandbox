c23898061f0f7b3598f921fe94adc912051f5c37
matovich <matovich@hotmail.com>
2022-10-11 13:43:30 -0500

    Initial Work

---
+++ WpfSandbox/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfSandbox
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ WpfSandbox/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WpfSandbox
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=products.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
